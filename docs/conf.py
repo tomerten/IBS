f5c6c8226bb6c962850f741e62ac51fda956f6c2
Tom Mertens <your.email@whatev.er>
2021-07-17 22:23:41 +0200

    docs issues

--- docs/conf.py
+++ docs/conf.py
@@ -83,10 +83,10 @@ read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
 breathe_projects = {}
 
 if read_the_docs_build:
+    subprocess.call("pip install poetry", shell=True)
     subprocess.call("poetry install", shell=True)
     # input_dir = '../CatCutifier'
     output_dir = ""
     # configureDoxyfile(input_dir, output_dir)
-    subprocess.call("pip install pandas", shell=True)
     subprocess.call("doxygen", shell=True)
     breathe_projects["ibs"] = output_dir + "xml"
